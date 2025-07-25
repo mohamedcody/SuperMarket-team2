a96c7a3f1ae5d4a89acf9c0d7d6bd2e2b1ce81b7
Mohamed Cody <mohamedcody18@gmail.com>
2025-07-25 23:29:29 +0300

    first commit : initialize Spring Boot SuperMarket project

---
+++ src/main/java/com/team2/supermaket/SupermaketApplication.java
@@ -0,0 +1,13 @@
+package com.team2.supermaket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermaketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermaketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/supermaket/SupermaketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.supermaket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermaketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
